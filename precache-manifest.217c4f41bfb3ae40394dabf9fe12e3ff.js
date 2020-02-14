a9ccd6a09eee0f8c54572328b672546d501048af
iamohsin <59395132+iamohsin@users.noreply.github.com>
2020-02-14 17:49:29 +0500

    Updates

---
+++ precache-manifest.217c4f41bfb3ae40394dabf9fe12e3ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ed2aa5be36d854e33c6f8e788af67d",
+    "url": "/Faceapp-git-V-0.1/index.html"
+  },
+  {
+    "revision": "00b826ca6b5a764077c1",
+    "url": "/Faceapp-git-V-0.1/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "99595eb7fba82516e312",
+    "url": "/Faceapp-git-V-0.1/static/css/main.5e41a273.chunk.css"
+  },
+  {
+    "revision": "00b826ca6b5a764077c1",
+    "url": "/Faceapp-git-V-0.1/static/js/2.9b07b10d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Faceapp-git-V-0.1/static/js/2.9b07b10d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99595eb7fba82516e312",
+    "url": "/Faceapp-git-V-0.1/static/js/main.2c017400.chunk.js"
+  },
+  {
+    "revision": "36dabe0cedcba26238f1",
+    "url": "/Faceapp-git-V-0.1/static/js/runtime-main.20a5eed3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/iamohsin/Faceapp-git-V-0.1/precache-manifest.18a47e54be0da96d4500b13188e282ec.js"
+  "/Faceapp-git-V-0.1/precache-manifest.217c4f41bfb3ae40394dabf9fe12e3ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iamohsin/Faceapp-git-V-0.1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Faceapp-git-V-0.1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
